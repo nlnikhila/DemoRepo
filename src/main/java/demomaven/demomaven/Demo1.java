b03b753dce1d300c1dfea72458aed305d0d34279
nlnikhila <nlnikhila@gmail.com>
2019-05-22 09:50:24 -0500

    My demo commit

---
+++ src/main/java/demomaven/demomaven/Demo1.java
@@ -0,0 +1,13 @@
+package demomaven.demomaven;
+
+
+
+
+public class Demo1 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("helloworld");
+	}
+
+}
---
+++ src/test/java/demomaven/demomaven/NewTest2.java
@@ -0,0 +1,10 @@
+package demomaven.demomaven;
+
+import org.testng.annotations.Test;
+
+public class NewTest2 {
+  @Test
+  public void f() {
+	  System.out.println("New Test2...");
+  }
+}
